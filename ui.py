98cb3a82897cb11555cccb2d1900ded61c6d424c
Pefatus <pefatus@gmail.com>
2026-01-10 06:05:34 +0530

    frontend 3

--- ui.py
+++ ui.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("kollam_intersection_risk.csv")
-    return df.head(1000)
+    df = pd.read_csv("kollam_top500_high_risk.csv")
+    return df.head(500)
 
 
 if "nodes_df" not in st.session_state:
@@ -24,26 +24,16 @@ header {visibility: hidden;}
 </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-st.title("Accident Prone Areas")
+st.title("Accident Hotzones")
 
 col1, col2 = st.columns([1, 5])
-location = streamlit_geolocation()
+user_location = streamlit_geolocation()
+st.session_state["lat"] = user_location["latitude"]
+st.session_state["lon"] = user_location["longitude"]
 
 @st.cache_data
 def create_map(df):
     m = folium.Map(location=[8.8932, 76.6141], zoom_start=10)
-
-    if location:
-        folium.CircleMarker(
-            location=[location["latitude"], location["longitude"]],
-            radius=5,
-            color="blue",
-            fill=True,
-            fill_color="blue",
-            fill_opacity=0.7,
-            popup="You are here"
-        ).add_to(m)
-
 
     for _, node in nodes_df.iterrows():
         folium.CircleMarker(
@@ -73,8 +63,8 @@ with col1:
     #no_of_int = st.number_input("No. of intersections", 10, 100, key="n")
 
     st.success(f"Selected Location:")
-    st.write(f"Latitude: {st.session_state.get("lat", "")}")
-    st.write(f"Longitude: {st.session_state.get("lon", "")}")
+    st.write(f"Latitude: {st.session_state.get("y", "")}")
+    st.write(f"Longitude: {st.session_state.get("x", "")}")
     if map_data and map_data.get("last_clicked"):
-        st.session_state["lat"] = map_data["last_clicked"]["lat"]
-        st.session_state["lon"] = map_data["last_clicked"]["lng"]
+        st.session_state["y"] = map_data["last_clicked"]["lat"]
+        st.session_state["x"] = map_data["last_clicked"]["lng"]
